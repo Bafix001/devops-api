e38342fcf1345bc8697ce5ba596f9d795251a393
Bafix001 <zibooumorou44@gmail.com>
2026-06-30 15:23:57 +0200

    feat: add styled frontend v1

--- project/index.js
+++ project/index.js
@@ -1,15 +1,56 @@
 const express = require("express");
-const app = express();
+const os = require("os");
 
+const app = express();
 const PORT = 3000;
 
-// route principale
 app.get("/", (req, res) => {
-  res.json({ message: "Hello DevOps 🚀" });
+  res.send(`
+    <html>
+      <head>
+        <title>DevOps API</title>
+        <style>
+          body {
+            background: #0f172a;
+            color: white;
+            font-family: Arial, sans-serif;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+          }
+          .card {
+            background: #1e293b;
+            padding: 30px;
+            border-radius: 12px;
+            box-shadow: 0 0 20px rgba(0,0,0,0.5);
+            text-align: center;
+          }
+          h1 {
+            color: #38bdf8;
+          }
+          p {
+            margin: 5px 0;
+          }
+          .version {
+            color: #22c55e;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="card">
+          <h1>🚀 DevOps Project</h1>
+          <p class="version">Version: v1</p>
+          <p>Hostname: ${os.hostname()}</p>
+          <p>Uptime: ${process.uptime().toFixed(2)} sec</p>
+          <p>Date: ${new Date()}</p>
+        </div>
+      </body>
+    </html>
+  `);
 });
 
-// lancer le serveur
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-``
